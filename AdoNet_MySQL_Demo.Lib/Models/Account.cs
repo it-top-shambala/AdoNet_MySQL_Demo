0c204cd9391e3375faa45ba3acf16d6d0b68f079
Starinin Andrey <starininandrey@gmail.com>
2022-09-16 21:31:22 +0300

    Add lib proj

---
+++ AdoNet_MySQL_Demo.Lib/Models/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace AdoNet_MySQL_Demo.Lib.Models;
+
+public class Account
+{
+    public int Id { get; set; }
+    public string Login { get; set; }
+    public string Password { get; set; }
+    public bool IsDelete { get; set; }
+}
---
+++ AdoNet_MySQL_Demo.Lib/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace AdoNet_MySQL_Demo.Lib.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
