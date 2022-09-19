e5221c0b822cfa3d12cd69f3d4aa18c96d80cb5e
Starinin Andrey <starininandrey@gmail.com>
2022-09-19 19:51:28 +0300

    Add DbConfig

---
+++ AdoNet_MySQL_Demo.Lib/DB/DbConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+
+namespace AdoNet_MySQL_Demo.Lib.DB;
+
+public record DbConfig
+{
+    public string Server { get; set; }
+    public string DataBase { get; set; }
+    public string User { get; set; }
+    public string Password { get; set; }
+
+    public override string ToString()
+    {
+        return $"Server={Server};Database={DataBase};Uid={User};Pwd={Password};";
+    }
+
+    public static DbConfig Import(string path)
+    {
+        var json = File.ReadAllText(path);
+        return JsonSerializer.Deserialize<DbConfig>(json);
+    }
+}
